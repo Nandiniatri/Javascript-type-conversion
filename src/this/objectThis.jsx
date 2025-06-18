29d87d683ffea66a75e177407928c7263589dda9
Nandiniatri <atrinandini8@gmail.com>
2025-06-18 20:28:35 +0530

    add new code again

--- src/this/objectThis.jsx
+++ src/this/objectThis.jsx
@@ -8,11 +8,23 @@
 // obj.greet();
 
 
-const person = {
-  name: "Nandini",
-  sayHi: function() {
-    console.log("Hi, I am " + this.name);
-  }
-};
+// const person = {
+//   name: "Nandini",
+//   sayHi: function() {
+//     console.log("Hi, I am " + this.name);
+//   }
+// };
 
-person.sayHi(); // "Hi, I am Nandini"
+// person.sayHi(); // "Hi, I am Nandini"
+
+
+
+function show() {
+  console.log(this.name);
+}
+
+const user = { name: "Nandini" };
+
+const boundShow = show.bind(user);
+boundShow();
+
