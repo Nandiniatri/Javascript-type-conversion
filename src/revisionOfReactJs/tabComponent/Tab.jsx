68bbd19b19fff10f0239129a39d991bcb18f78ab
Nandiniatri <atrinandini8@gmail.com>
2025-06-25 17:00:11 +0530

    make a tabComponent without id

--- src/revisionOfReactJs/tabComponent/Tab.jsx
+++ src/revisionOfReactJs/tabComponent/Tab.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
 const tabData = [
-    { id: 1, title: 'Tab 1', content: 'Tab 1 Content' },
-    { id: 2, title: 'Tab 2', content: 'Tab 2 Content' },
-    { id: 3, title: 'Tab 3', content: 'Tab 3 Content' }
+    { title: 'Tab 1', content: 'Tab 1 Content' },
+    { title: 'Tab 2', content: 'Tab 2 Content' },
+    { title: 'Tab 3', content: 'Tab 3 Content' }
 ]
 
 const TabComponents = () => {
-    const [tabCom , setTabCom] =  useState([]);
+    const [tabCom, setTabCom] = useState(tabData[0].content);
 
     const handleTab = (item) => {
         console.log(item.content);
@@ -18,7 +18,7 @@ const TabComponents = () => {
         <>
             {tabData.map((item) => {
                 return (
-                    <div key={item.id}>
+                    <div key={item.title}>
                         <button onClick={() => handleTab(item)}>{item.title}</button>
                     </div>
                 )
