6f3cd988fa8c805e3002ef9da43fde73f8d63493
Nandiniatri <atrinandini8@gmail.com>
2025-06-25 19:51:15 +0530

    make props

---
+++ src/revisionOfReactJs/props/Greeting.jsx
@@ -0,0 +1,9 @@
+const Greeting = ({ title }) => {
+    return (
+        <>
+            <h2>Name = {title}</h2>
+        </>
+    )
+}
+
+export default Greeting;
--- src/revisionOfReactJs/revision.jsx
+++ src/revisionOfReactJs/revision.jsx
@@ -1,16 +1,20 @@
+import Greeting from "./props/Greeting";
 import ReactMemo from "./reactMemo/ReactMemo";
 import TabComponents from "./tabComponent/Tab";
 import Timer from "./useEffect/Timer";
 import Counter from "./useState/Counter";
 
 const RevisionOfReact = () => {
-    return(
+    const title = 'Nandini';
+
+    return (
         <>
             <h1>Revision Of React Js</h1>
             {/* <ReactMemo /> */}
             <TabComponents />
             <Counter />
             <Timer />
+            <Greeting title={title} />
         </>
     )
 }
