78cacfd930cf50c47cdbf56619bde6ef86abf187
Nandiniatri <atrinandini8@gmail.com>
2025-06-27 23:43:11 +0530

    make fetch

--- src/revisionOfReactJs/fetchData/FetchData.jsx
+++ src/revisionOfReactJs/fetchData/FetchData.jsx
@@ -1,7 +1,14 @@
 const FetchData = () => {
 
-    const fetchProduct = () => {
-        
+
+    const fetchProduct = async() => {
+        try {
+            const response = await fetch('https://dummyjson.com/products');
+            const result = await response.json();
+        } catch (error) {
+            console.log('Network is slow');   
+        }
+
     }
     return (
         <div>
