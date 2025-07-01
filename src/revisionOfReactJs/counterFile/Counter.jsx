303d3ae1913851d32d598a0c68e3757f629dcb42
Nandiniatri <atrinandini8@gmail.com>
2025-07-01 15:45:53 +0530

    functionally

--- src/revisionOfReactJs/counterFile/Counter.jsx
+++ src/revisionOfReactJs/counterFile/Counter.jsx
@@ -4,11 +4,11 @@ const Counter = () => {
     const [count , setCount] = useState(0);
 
     const handlePlus = () => {
-
+        setCount(count + 1);
     }
 
     const handleMinus = () => {
-
+        setCount(count - 1);
     }
 
     return (
