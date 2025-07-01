36d81606dd7101382fa5802acf9e4697769fa7cd
Nandiniatri <atrinandini8@gmail.com>
2025-07-01 15:43:06 +0530

    add counter

--- src/revisionOfReactJs/counterFile/Counter.jsx
+++ src/revisionOfReactJs/counterFile/Counter.jsx
@@ -2,6 +2,14 @@ import { useState } from "react";
 
 const Counter = () => {
     const [count , setCount] = useState(0);
+
+    const handlePlus = () => {
+
+    }
+
+    const handleMinus = () => {
+
+    }
     
     return (
         <div>
