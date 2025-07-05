3d3498718725ca76c64296dc45bce8a05a7fe122
Nandiniatri <atrinandini8@gmail.com>
2025-07-05 20:47:24 +0530

    header css done

--- src/revisionOfReactJs/luxuryFurnitureSellingWebpage/header/Header.jsx
+++ src/revisionOfReactJs/luxuryFurnitureSellingWebpage/header/Header.jsx
@@ -3,11 +3,12 @@ import Button from "../../../component/Button";
 import { IoIosSearch } from "react-icons/io";
 import { IoCartOutline } from "react-icons/io5";
 import { LiaUser } from "react-icons/lia";
+import './Header.css'
 
 const Header = () => {
     return (
         <header>
-            <Image src='https://cdn.pixabay.com/photo/2023/01/31/01/50/crown-7756927_1280.png' className=""/>
+            <Image src='https://cdn.pixabay.com/photo/2023/01/31/01/50/crown-7756927_1280.png' className="image-header-img" />
 
             <nav>
                 <a href="">Home</a>
@@ -17,10 +18,10 @@ const Header = () => {
                 <a href="">Find Store</a>
             </nav>
 
-            <div>
-                <Button><IoIosSearch /></Button>
-                <Button><IoCartOutline /></Button>
-                <Button><LiaUser /></Button>
+            <div className="header-button-div">
+                <Button className="header-btn1"><IoIosSearch className="header-icon1" /></Button>
+                <Button className="header-btn2"><IoCartOutline className="header-icon2" /></Button>
+                <Button className="header-btn3"><LiaUser className="header-icon3" /></Button>
             </div>
         </header>
     )
