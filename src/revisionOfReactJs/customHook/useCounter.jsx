21161d947b43e67f2c4a0bdfb8bc04c685f9f30f
Nandiniatri <atrinandini8@gmail.com>
2025-07-09 21:29:07 +0530

    make a useCounter

---
+++ src/revisionOfReactJs/customHook/useCounter.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+const useCounter = () => {
+    const [count, setCount] = useState();
+
+    const handlePrev = () => {
+
+    }
+
+    const handleNext = () => {
+
+    }
+
+    return {}
+}
+
+export default useCounter;
