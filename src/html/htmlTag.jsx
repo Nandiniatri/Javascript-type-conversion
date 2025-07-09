e66e21f9eea7652c7c3f5f2293f341dd5a9fed72
Nandiniatri <atrinandini8@gmail.com>
2025-07-09 21:33:16 +0530

    dymanic data with img

--- src/html/htmlTag.jsx
+++ src/html/htmlTag.jsx
@@ -1,4 +1,4 @@
-{/* <></> html tags  */}
+{/* <></> html tags  */ }
 
 
 // {/* <div>This is a nonsence things</div> */} html element
@@ -7,6 +7,7 @@ const HtmlTag = () => {
     return (
         <>
             <p>This is My Show</p>
+            <img src="https://miro.medium.com/v2/resize:fit:960/1*o2QkJWlZWqtDa-g3luWT1Q.jpeg" alt="" />
         </>
     )
 }
