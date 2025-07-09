e98a40b0de057e1919c4ea8066706bd32f1bd656
Nandiniatri <atrinandini8@gmail.com>
2025-07-09 21:33:42 +0530

    dymanic data

--- src/html/htmlTag.jsx
+++ src/html/htmlTag.jsx
@@ -7,7 +7,7 @@ const HtmlTag = () => {
     return (
         <>
             <p>This is My Show</p>
-            <img src="https://miro.medium.com/v2/resize:fit:960/1*o2QkJWlZWqtDa-g3luWT1Q.jpeg" alt="" />
+            <img src="https://miro.medium.com/v2/resize:fit:960/1*o2QkJWlZWqtDa-g3luWT1Q.jpeg" alt="romotic-img" />
         </>
     )
 }
