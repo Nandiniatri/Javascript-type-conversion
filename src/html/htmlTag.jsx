735714430ba3d243b89c5f557b06d34db6e7d2ce
Nandiniatri <atrinandini8@gmail.com>
2025-07-11 23:24:36 +0530

    create a toogle custom hook

--- src/html/htmlTag.jsx
+++ src/html/htmlTag.jsx
@@ -8,6 +8,7 @@ const HtmlTag = () => {
         <>
             <p>This is My Show</p>
             <img src="https://miro.medium.com/v2/resize:fit:960/1*o2QkJWlZWqtDa-g3luWT1Q.jpeg" alt="romotic-img" />
+            <button>Hello My Name is Nandini</button>
         </>
     )
 }
