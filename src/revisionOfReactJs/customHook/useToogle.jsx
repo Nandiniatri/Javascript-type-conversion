5f8707272e74ef8ab8cf9045d3a26a20efd5aba4
Nandiniatri <atrinandini8@gmail.com>
2025-07-11 23:23:11 +0530

    create a toogle custom hook

--- src/revisionOfReactJs/customHook/useToogle.jsx
+++ src/revisionOfReactJs/customHook/useToogle.jsx
@@ -3,7 +3,12 @@ import { useState } from "react";
 const useToogle = (initialValue = false) => {
 
     const [value, setValue] = useState(initialValue);
-    
+
+    const toogle = () => {
+        setValue(prev => !prev);
+    }
+
+    return [toogle, value];
 }
 
 export default useToogle;
