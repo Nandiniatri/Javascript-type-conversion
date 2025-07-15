1edca05a3535bf9c82e2a5ac069db6ad8032784c
Nandiniatri <atrinandini8@gmail.com>
2025-07-15 16:55:39 +0530

    practices a fetching api with fetch

--- src/reactRevision/Fetch.jsx
+++ src/reactRevision/Fetch.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+
 const FetchApis = () => {
+    const [data, setData] = useState([]);
+
+    const fetchProductApi = () => {
+        try {
+            const response = fetch('');
+            const result = response.json();
+            setData(result);
+        } catch (error) {
+            console.log('Network Slow');   
+        }
+    }
+
+    useEffect(() => {
+        fetchProductApi();
+    },[])
+
     return (
         <div>
-
+            
         </div>
     )
 }
