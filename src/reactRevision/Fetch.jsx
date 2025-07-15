fe1c570acb9bad74441e05f63a5a07aab102d312
Nandiniatri <atrinandini8@gmail.com>
2025-07-15 18:00:31 +0530

    filter and search implementation in product data

--- src/reactRevision/Fetch.jsx
+++ src/reactRevision/Fetch.jsx
@@ -3,27 +3,39 @@ import { useEffect, useState } from "react";
 const FetchApis = () => {
     const [data, setData] = useState([]);
     console.log(data);
-    
+    const [searchValue, setSearchValue] = useState('');
 
-    const fetchProductApi = async() => {
+
+    const fetchProductApi = async () => {
         try {
             const response = await fetch('https://dummyjson.com/products');
             console.log(response);
-            
+
             const result = await response.json();
             setData(result.products);
         } catch (error) {
-            console.log('Network Slow');   
+            console.log('Network Slow');
         }
     }
 
     useEffect(() => {
         fetchProductApi();
-    },[])
+    }, [])
+
+    const handleSearch = (event) => {
+        console.log(event.target.value);
+        setSearchValue(event.target.value);
+    }
+
+    const filterData = data.filter((item) => {
+        return item.title.toLowerCase().includes(searchValue.toLowerCase())
+    })
 
     return (
         <div>
-            {data.map((item) => {
+            <input type="search" onChange={handleSearch} value={searchValue}/>
+
+            {filterData.map((item) => {
                 console.log(item);
                 return (
                     <div>
