8a9222f717ce0fd4f9fb1073370c4180f71a6edc
Nandiniatri <atrinandini8@gmail.com>
2025-07-16 21:22:46 +0530

    exmaple of axiosData

--- src/reactRevision/Axios.jsx
+++ src/reactRevision/Axios.jsx
@@ -1,6 +1,19 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const AxiosData = () => {
+    const [data , setData] = useState([]);
+    
+    const fetchWithAxios = async() => {
+        const response = await axios('https://dummyjson.com/users');
+        const result = await response.json();
+        console.log(result); 
+    }
+
+    useEffect(() => {
+        fetchWithAxios();
+    },[])
+
     return(
         <div>
             
