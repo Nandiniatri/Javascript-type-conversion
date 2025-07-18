ea0696cbd7b5d0c61de7bebe515e5e38571a112c
Nandiniatri <atrinandini8@gmail.com>
2025-07-18 16:48:26 +0530

    make a component of select the options

--- src/reactRevision/SelectTheOption.jsx
+++ src/reactRevision/SelectTheOption.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
 const SelectTheOptions = () => {
+
+    const [selectName , setSelectedName] = useState([]);
+
+    const handleSelect = (event) => {
+        console.log(event.target.value); 
+        const values = event.target.value;  
+        setSelectedName(values);
+    }
+
     return (
         <>
-            <select>
+            <select name="Name" onChange={handleSelect}>
                 <option>Nandini Atri</option>
                 <option>Vanshika</option>
                 <option>Qwerty</option>
             </select>
+
+            <p>{selectName}</p>
         </>
     )
 }
