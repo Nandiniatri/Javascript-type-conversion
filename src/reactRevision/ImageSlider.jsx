47761172602115e18f607eaefcaed994cb8179f9
Nandiniatri <atrinandini8@gmail.com>
2025-07-18 17:01:01 +0530

    make a imageSlide

--- src/reactRevision/ImageSlider.jsx
+++ src/reactRevision/ImageSlider.jsx
@@ -1,8 +1,14 @@
-
+const imageData = [
+    {id:1 , src:'https://cdn.pixabay.com/photo/2024/05/22/21/51/dog-8781844_1280.jpg' , alt:'dog1'},
+    {id:2 , src:'https://cdn.pixabay.com/photo/2022/08/17/15/46/labrador-7392840_1280.jpg' , alt:'dog2'},
+    {id:3 , src:'https://cdn.pixabay.com/photo/2010/12/13/10/13/labrador-retriever-2548_1280.jpg' , alt:'dog3'},
+    {id:4 , src:'https://cdn.pixabay.com/photo/2016/04/19/17/23/dog-1339100_1280.jpg' , alt:'dog4'}
+]
 
 const ImageSlider = () => {
     return (
         <>
+        
         </>
     )
 }
